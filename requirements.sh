f52a82050dd2c434401d1ac864b58955abb3182a
jjonhwa <53552847+jjonhwa@users.noreply.github.com>
2021-11-17 11:39:58 +0900

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -5,8 +5,6 @@ pip install datasets==1.5.0
 pip install transformers==4.5.0
 pip install tqdm==4.41.1
 pip install pandas==1.1.4
-pip install scikit-learn==0.24.1
-pip install konlpy==0.5.2
 
 # faiss install (if you want to)
 # pip install faiss-gpu
